e45fe5d8a956b0968ed5fd0c59dd9ebeb9e0ac24
kanishksaini12 <68396302+kanishksaini12@users.noreply.github.com>
2021-03-16 23:51:53 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS studentData;
+
+CREATE TABLE studentData(
+    Name VARCHAR(20),
+    Rollno INTEGER,
+    Address VARCHAR(50),
+    Mobile VARCHAR(20),
+    PAN_number VARCHAR(10)
+);
+
+INSERT INTO studentData VALUES('Kanishk', 2020, 'Pune', '7426004449', 'ABCDE12345');
+INSERT INTO studentData VALUES('Anurag', 2032, 'Punjab', '9654368725', 'GHIJK67890');
+INSERT INTO studentData VALUES('Rajesh',1045,'Akola','178556789','ABEPV113F');
+INSERT INTO studentData VALUES('Anubhav',1013,'Delhi','123452569','ABFPK127K');
+INSERT INTO studentData VALUES('Sangita',2032,'Pune','123743789','LAG357SF57');
+INSERT INTO studentData VALUES('Avichal',2028,'Ahemdabad','984456789','RI63SA921A');
+INSERT INTO studentData VALUES('Aman',2001,'Kota','123267789','ABIPY172B');
+INSERT INTO studentData VALUES('Rahul',1021,'Mumbai','123456654','HNJ325RT96');
+INSERT INTO studentData VALUES('Sheetal',2034,'Chennai','123695789','ABLG245672');
